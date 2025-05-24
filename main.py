8f1d7a8948a316427f3e0e7e6d211ac804705edd
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-05-24 17:43:09 +0530

    - added threading to main.py - added an empty README - deleted what to do.txt

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
 import time
+import threading
 import keyboard
 import ctypes
 
-key = keyboard.read_key(suppress=True)
+key = None
+
+def key_listener_thread_func():
+    global key
+    while True:
+        key = keyboard.read_key(suppress=True)
+
+listener_thread = threading.Thread(target=key_listener_thread_func, daemon=True)
+listener_thread.start()
 
 while key != "`":
     
     print(ctypes.windll.user32.FindWindowW(None, "smth"))
-
-    key = keyboard.read_key(suppress=True)
 
     if key == "alt":
         script_path = os.path.abspath(__file__)
@@ -17,4 +24,4 @@ while key != "`":
 
         os.system('start "smth"')
     
-    time.sleep(0.05)
+    time.sleep(0.01)
