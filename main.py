c09477734bbf048cc5072c7be14827c7e25ab9df
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-05-24 14:15:10 +0530

    - trying to implement multi window management

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import os
+import time
+import keyboard
+import ctypes
 
+key = keyboard.read_key(suppress=True)
 
-os.startfile("main_program.py")
+while key != "`":
+    
+    print(ctypes.windll.user32.FindWindowW(None, "smth"))
+
+    key = keyboard.read_key(suppress=True)
+
+    if key == "alt":
+        script_path = os.path.abspath(__file__)
+        script_directory = os.path.dirname(script_path)
+
+        os.system('start "smth"')
+    
+    time.sleep(0.05)
