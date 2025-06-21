ac8cc919567a05a20f69017b801e559a049a71ea
Beta-Verse-Hub <baibhavkumarjha1@gmail.com>
2025-06-21 20:47:45 +0530

    - adding a json for config

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-config = {
-    "Main Path"                : "C:/",
-    "Calculator Color 1 Red"   : "0",
-    "Calculator Color 1 Green" : "255",
-    "Calculator Color 1 Blue"  : "0",
-    "Calculator Color 2 Red"   : "255",
-    "Calculator Color 2 Green" : "0",
-    "Calculator Color 2 Blue"  : "0"
-}
+types = ("unchangable",
+         "rgb",
+         "text",
+         "value")
+
+def rgb_input():
+    pass
